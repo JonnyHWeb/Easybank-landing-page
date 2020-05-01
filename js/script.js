283d4dc0459210ee9b84d6280161b0ff9ce23093
Jonny Hunter <hunter.jonny@yahoo.co.uk>
2020-05-01 11:41:59 +0100

    mobile nav add transparency

--- js/script.js
+++ js/script.js
@@ -7,8 +7,10 @@ function navStateChange() {
   
   if(newSrc==navClosed) {
     document.querySelector(".main-nav--expanded").className = "main-nav";
+    // document.querySelector(".main-nav").style.display = "none";
   }
   else {
     document.querySelector(".main-nav").className = "main-nav main-nav--expanded";
+    // document.querySelector(".main-nav").style.display = "block";
   }
 }
